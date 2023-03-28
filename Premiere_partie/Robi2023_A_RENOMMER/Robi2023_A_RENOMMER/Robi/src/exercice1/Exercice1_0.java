17abce245819abd8dc8143b23145a116bcd455ba
Tanguy Le Goff <tanguy.legoff@univ-brest.fr>
2023-03-28 15:23:42 +0200

    Début exo 1

--- Premiere_partie/Robi2023_A_RENOMMER/Robi2023_A_RENOMMER/Robi/src/exercice1/Exercice1_0.java
+++ Premiere_partie/Robi2023_A_RENOMMER/Robi2023_A_RENOMMER/Robi/src/exercice1/Exercice1_0.java
@@ -1,8 +1,10 @@
 package exercice1;
 
-import java.awt.Dimension;
+import java.awt.*;
+
 import graphicLayer.GRect;
 import graphicLayer.GSpace;
+import tools.Tools;
 
 public class Exercice1_0 {
 	GSpace space = new GSpace("Exercice 1", new Dimension(200, 150));
@@ -11,6 +13,33 @@ public class Exercice1_0 {
 	public Exercice1_0() {
 		space.addElement(robi);
 		space.open();
+
+		int currentX = 0;
+		int currentY = 0;
+
+		int wait_time = 2;
+		while (true) {
+			for (int i = 0; i < space.getWidth() - robi.getWidth(); i++) {
+				currentX = i;
+				robi.setPosition(new Point(currentX, currentY));
+				Tools.sleep(wait_time);
+			}
+			for (int i = 0; i < space.getHeight() - robi.getHeight(); i++) {
+				currentY = i;
+				robi.setPosition(new Point(currentX, currentY));
+				Tools.sleep(wait_time);
+			}
+			for (int i = 0; i < space.getWidth() - robi.getWidth(); i++) {
+				currentX--;
+				robi.setPosition(new Point(currentX, currentY));
+				Tools.sleep(wait_time);
+			}
+			for (int i = 0; i < space.getHeight() - robi.getHeight(); i++) {
+				currentY--;
+				robi.setPosition(new Point(currentX, currentY));
+				Tools.sleep(wait_time);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
