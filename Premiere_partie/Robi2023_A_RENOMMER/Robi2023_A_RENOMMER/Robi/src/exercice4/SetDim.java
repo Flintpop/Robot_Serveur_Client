445c3259b82476b3e63f8c191b08f784e5f9575c
Hippolyte Jean <hippolyte.jean@etudiant.univ-brest.fr>
2023-03-29 16:50:53 +0200

    Oubli de setDim

--- Premiere_partie/Robi2023_A_RENOMMER/Robi2023_A_RENOMMER/Robi/src/exercice4/SetDim.java
+++ Premiere_partie/Robi2023_A_RENOMMER/Robi2023_A_RENOMMER/Robi/src/exercice4/SetDim.java
@@ -1,5 +1,8 @@
 package exercice4;
 
+import java.awt.Dimension;
+
+import graphicLayer.GBounded;
 import stree.parser.SNode;
 
 public class SetDim implements Command {
@@ -7,7 +10,12 @@ public class SetDim implements Command {
 	@Override
 	public Reference run(Reference ref, SNode method) {
 
-		
+		if(ref.receiver instanceof GBounded) {
+			GBounded b = (GBounded) ref.receiver;
+			Dimension d = new Dimension(Integer.parseInt(method.get(2).contents()), Integer.parseInt(method.get(3).contents()));
+			b.setDimension(d);
+			
+		}
 
 		return null;
 	}
