ca30eda2381da8f5de2f372d88cf4558307f1537
abdelaziz amil <abdelaziz.amil@hotmail.com>
2023-03-30 13:24:41 +0200

    ajout des Classes à envoyer à travers la socket

---
+++ Premiere_partie/Robi2023_A_RENOMMER/Robi2023_A_RENOMMER/Robi/src/exercice4/DataCS.java
@@ -0,0 +1,22 @@
+package exercice4;
+
+public class DataCS {
+    String cmd = null;
+    String txt = null;
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+}
---
+++ Premiere_partie/Robi2023_A_RENOMMER/Robi2023_A_RENOMMER/Robi/src/exercice4/DataSC.java
@@ -0,0 +1,31 @@
+package exercice4;
+
+public class DataSC {
+    String cmd = null;
+    String txt = null;
+    String im = null;
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+    public String getTxt() {
+        return txt;
+    }
+
+    public void setTxt(String txt) {
+        this.txt = txt;
+    }
+
+    public String getIm() {
+        return im;
+    }
+
+    public void setIm(String im) {
+        this.im = im;
+    }
+}
